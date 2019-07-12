317408bfbb068606ca80f647b02b1d9a38657256
Remo Oser <remo.oser@osisa.com>
2019-07-12 13:57:30 +0200

    adding dist

--- pub.bat
+++ pub.bat
@@ -1,5 +1,5 @@
 rmdir /Q /S publish
 rmdir /Q /S dist
 dotnet publish ./src/minationalrot.sln -c Release -o ./publish
-copy publish\minationalrot\dist dist\
+xcopy publish\minationalrot\dist dist\
 pause
