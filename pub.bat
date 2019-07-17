8a88c1f5c8d508d64aaaaa41e6523cd917f77012
Remo Oser <remo.oser@osisa.com>
2019-07-17 19:25:56 +0200

    changing to default agents2019N

--- pub.bat
+++ pub.bat
@@ -1,5 +1,9 @@
 rmdir /Q /S publish
+git rm docs -r
 rmdir /Q /S docs
 dotnet publish ./src/minationalrot.sln -c Release -o ./publish
 xcopy publish\minationalrot\dist docs\ /s
+git add docs
+git commit -m "deploy ***NO_CI***"
+git push
 pause
