b267928ab233e13bedf91e619015c84bb06510dc
Remo Oser <remo.oser@osisa.com>
2019-07-18 21:25:49 +0200

    upload working ***NO_CI***

--- pub.bat
+++ pub.bat
@@ -1,9 +1,9 @@
-rmdir /Q /S publish
-git rm docs -r
-rmdir /Q /S docs
-dotnet publish ./src/minationalrot.sln -c Release -o ./publish
-xcopy publish\minationalrot\dist docs\ /s
-git add docs
-git commit -m "deploy ***NO_CI***"
-git push
-pause
+rmdir /Q /S c:\publish
+dotnet publish ./src/minationalrot.sln -c Release -o c:\publish
+cd c:\publish\minationalrot\dist
+git init
+git add .
+git commit -m "Push force deploy ***NO_CI***"
+git remote add origin https://github.com/minationalrot/miNationalrot.git
+git push --force origin master:gh-pages
+pause 
