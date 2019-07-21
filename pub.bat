ecda141d20170f49d506e04968ab8699a535a7ca
Remo Oser <remo.oser@osisa.com>
2019-07-21 19:37:37 +0200

    24. try azure deploy

--- pub.bat
+++ pub.bat
@@ -4,6 +4,6 @@ cd c:\publish\minationalrot\dist
 git init
 git add .
 git commit -m "Push force deploy ***NO_CI***"
-git remote add origin https://github.com/minationalrot/miNationalrot.git
+git remote add origin https://RemoOser:b05095333ffd54b99c03390cca83b9796b600e03@github.com/minationalrot/miNationalrot.git
 git push --force origin master:gh-pages
 pause 
