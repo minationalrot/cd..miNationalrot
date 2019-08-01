68bacfee39878db34d5877c42b8d2aa57dcdc9d4
Remo Oser <remo.oser@osisa.com>
2019-08-01 10:45:37 +0200

    reset to old index html again

--- src/miNationalrot/Program.cs
+++ src/miNationalrot/Program.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿
+
+using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace miNationalrot
 {
@@ -18,29 +11,8 @@ namespace miNationalrot
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
     }
 }
-
-
-//using Microsoft.AspNetCore.Blazor.Hosting;
-
-//namespace miNationalrot
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            CreateHostBuilder(args).Build().Run();
-//        }
-
-//        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-//            BlazorWebAssemblyHost.CreateDefaultBuilder()
-//                .UseBlazorStartup<Startup>();
-//    }
-//}
